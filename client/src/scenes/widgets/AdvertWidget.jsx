1a66e0120ee4a5ad6320b0f3828a7525d20ac758
Rutwik Deshmukh <rutwikdesh28@gmail.com>
2023-02-07 19:59:58 +0530

    added code from Donut repo

---
+++ client/src/scenes/widgets/AdvertWidget.jsx
@@ -0,0 +1,37 @@
+import { Typography, useTheme } from "@mui/material";
+import FlexBetween from "components/FlexBetween";
+import WidgetWrapper from "components/WidgetWrapper";
+
+const AdvertWidget = () => {
+  const { palette } = useTheme();
+  const dark = palette.neutral.dark;
+  const main = palette.neutral.main;
+  const medium = palette.neutral.medium;
+
+  return (
+    <WidgetWrapper>
+      <FlexBetween>
+        <Typography color={dark} variant="h5" fontWeight="500">
+          Sponsored
+        </Typography>
+        <Typography color={medium}>Create Ad</Typography>
+      </FlexBetween>
+      <img
+        width="100%"
+        height="auto"
+        alt="advert"
+        src="assets/sponsored.jpg"
+        style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
+      />
+      <FlexBetween>
+        <Typography color={main}>MusicHub</Typography>
+        <Typography color={medium}>musichub.com</Typography>
+      </FlexBetween>
+      <Typography color={medium} m="0.5rem 0">
+        Listen to all your faviourite without any ad breaks only on Music Hub.
+      </Typography>
+    </WidgetWrapper>
+  );
+};
+
+export default AdvertWidget;
